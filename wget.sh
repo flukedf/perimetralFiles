49e775a1de5d2dbe93a063c140c9348bf8eaf0ce
flukedf <fluke.df@gmail.com>
2018-02-23 11:28:53 -0600

    AGREGANDO CHMOD

--- wget.sh
+++ wget.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
  wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/V230218.hex
-
+chmod 777 V230218.hex
 merge-sketch-with-bootloader.lua /root/V230218.hex
 
 run-avrdude /root/V230218.hex
