1a66fc4966d1f18404eaacb32ac12408fa1f369e
flukedf <fluke.df@gmail.com>
2018-02-23 14:27:03 -0600

    Add reboot in upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -4,19 +4,26 @@
 #Author: Gonzalo Silverio  -> gonzasilve@gmail.com
 #Archivo: script2.sh
 #pedir el dato al usuario
-echo 'Introduzca Version de Firmware:'
+echo '----------------------Introduzca Version de Firmware:-----------------------------'
 #leer el dato del teclado y guardarlo en la variable de usuario var1
-echo "Versiones Disponibles"
+echo "----------------------Versiones Disponibles----------------------"
+echo ///////////////////////////
 echo "screen.hex"
 echo "PerimetralScreen.hex"
 echo "V230218.hex"
+echo/////////////////////////////
+echo
+echo
+echo
+echo
 read var1
 #Mostrar el valor de la variable de usuario
+echo Version Seleccionada:
+echo
 echo $var1
-echo $var1
-echo $var1
-echo $var1
-
+echo 
+echo
+echo
 
 
 wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/"$var1".hex
@@ -28,3 +35,7 @@ run-avrdude /root/"$var1".hex
 echo
 #Avisar al usuario que se ha terminado de ejecutar el script 
 echo ---------Fin del script.-------------
+echo
+echo
+echo ---------Reiniciando el Sistema-------------
+reboot
