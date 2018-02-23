35e6e18458558c8f641c9a1e6ac219f98908efd0
flukedf <fluke.df@gmail.com>
2018-02-23 11:12:22 -0600

    add wget

---
+++ wget.sh
@@ -0,0 +1,6 @@
+  #!/bin/bash
+ wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/V230218.hex
+
+merge-sketch-with-bootloader.lua /root/V230218.hex
+
+run-avrdude /root/V230218.hex
