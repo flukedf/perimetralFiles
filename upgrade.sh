043a3ed5e02fb8e5a30cff48e9ced6d2fcad2506
flukedf <fluke.df@gmail.com>
2018-07-15 10:26:13 -0500

    add watchcat

--- upgrade.sh
+++ upgrade.sh
@@ -101,6 +101,12 @@ uci set system.@system[0].zonename="America/Mexico City"
 uci set system.@system[0].timezone=CST6CDT,M4.1.0,M10.5.0
 uci set system.vendor.zonename="America/Mexico City"
 uci set system.vendor.timezone=CST6CDT,M4.1.0,M10.5.0
+uci set system.@watchcat[0]=watchcat
+uci set system.@watchcat[0].pinghost=8.8.8.8
+uci set system.@watchcat[0].mode=ping
+uci set system.@watchcat[0].pingperiod=10m
+uci set system.@watchcat[0].period=10m
+uci set system.@watchcat[0].forcedelay=240
 uci commit system
 luci-reload
 sudo /etc/init.d/atd start
