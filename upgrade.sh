e273b172b0462321f50bbdb519685e24c9d82835
flukedf <fluke.df@gmail.com>
2018-08-22 10:08:50 -0500

    Cambios en reset

--- upgrade.sh
+++ upgrade.sh
@@ -66,7 +66,7 @@ chmod 777 service.py
 chmod 777 "$var1".hex
 
 echo '10 * * * * "reset-mcu"'>>/etc/crontabs/root
-echo '10 * * * * "python /root/checkBridge.py"'>>/etc/crontabs/root
+echo '15 * * * * "python /root/checkBridge.py"'>>/etc/crontabs/root
 echo '00 12 * * * "reboot"'>>/etc/crontabs/root
 echo '00 00 * * * "reboot"'>>/etc/crontabs/root
 ###########################
