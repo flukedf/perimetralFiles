a760aad87b3a5b71735d403f78f07e66bb5c0ea3
flukedf <fluke.df@gmail.com>
2018-11-05 22:33:54 -0600

    Update checkInit.py

--- checkInit.py
+++ checkInit.py
@@ -1,18 +1,14 @@
 # Test to (un)set pin 12 and 13 on the Arduino Yun.
 # H.Zimmerman, 09-12-2014.
 # henszimmerman@gmail.com
- 
 import sys
 import os
-
 
 sys.path.insert(0, '/usr/lib/python2.7/bridge')
  
 from time import sleep
  
 from bridgeclient import BridgeClient as bridgeclient
-
-os.system('date')
 
 status = 0
 carry = 0
@@ -26,15 +22,13 @@ try:
 		carry = carry + status
 		sleep(7.5)
 		#print ("Valor Carry",carry)	
-
-
 except:
+	os.system('date')
 	print("No se incio MCU","Status",status)
 
-
 if carry != 0:
-	#print ("MCU iniciado correctamente")
-else:	
+	pass#print ("MCU iniciado correctamente")
+else:
 	print("MCU se reiniciara nuevamente","MCU didn't start")
 	os.system('reset-mcu')
 	try:
@@ -45,11 +39,8 @@ else:
 		carry = carry + status
 		#print ("Valor Carry 2",carry)	
 		if carry != 0:
-			#print ("MCU iniciado correctamente")
-
+			pass#print ("MCU iniciado correctamente")
 	except:
 		print("No se incio MCU 2","Valor Status 2",status,"Reboot")
 		os.system('reboot')
-
-		#sleep(30)
-    	
+	#sleep(30)
