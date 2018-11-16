a4b61e244012873bde41a5a27c9df2969141f3ed
flukedf <fluke.df@gmail.com>
2018-11-16 11:54:55 -0600

    Update unix.sh

--- unix.sh
+++ unix.sh
@@ -4,6 +4,6 @@ rm unix.sh
 wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/unix.sh
 chmod 777 unix.sh
 #reboot
-mv checkInit.py checkInit.pyy
+sudo mv checkInit.py checkInit.pyy
 
 
