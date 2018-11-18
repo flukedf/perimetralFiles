1010a1309dfae5cafdf1bd57d5577d81fb5353ac
flukedf <fluke.df@gmail.com>
2018-11-17 23:52:12 -0600

    Update unix.sh

--- unix.sh
+++ unix.sh
@@ -6,4 +6,7 @@ chmod 777 unix.sh
 #reboot
 sudo mv checkInit.py checkInit.pyy
 
+rm postData.py
 
+wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/unix.sh
+chmod 777 postData.sh
