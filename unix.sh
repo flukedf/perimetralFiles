4854870c3244f491898c48e37fff9ea5bf652cdb
flukedf <fluke.df@gmail.com>
2018-11-17 23:58:28 -0600

    Update unix.sh

--- unix.sh
+++ unix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 rm unix.sh
-pause 5
+sleep 5
 wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/unix.sh
 chmod 777 unix.sh
 #reboot
@@ -10,4 +10,4 @@ sudo mv checkInit.py checkInit.pyy
 rm postData.py
 
 wget --no-check-certificate https://raw.githubusercontent.com/flukedf/perimetralFiles/master/unix.sh
-chmod 777 postData.sh
+chmod 777 postData.py
